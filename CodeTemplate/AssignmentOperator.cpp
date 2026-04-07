5a92de6b999f704a7e4b5b388a34b8a3fc6f441e
daTahere <adamhuy123@yahoo.com>
2026-04-07 16:08:07 -0700

    Template: fix comments

--- CodeTemplate/AssignmentOperator.cpp
+++ CodeTemplate/AssignmentOperator.cpp
@@ -12,89 +12,89 @@
 
 */
 
-//#include <iostream>
-//
-//using namespace std;
-//
-//const char defaultStr[] = "Hello world";
-//
-//class MyString {
-//
-//private:
-//	char* str;
-//
-//public:
-//
-//	MyString(const char* input = defaultStr) {
-//
-//		if (input == nullptr || strlen(input) == 0) {
-//			input = defaultStr;
-//		}
-//
-//		int len = strlen(input) + 1;
-//		str = new char[len];
-//		strcpy_s(str, len, input);
-//
-//	};
-//
-//	// Copy Constructor
-//	MyString(const MyString& s) {
-//		int len = strlen(s.str) + 1;
-//		str = new char[len];
-//
-//		strcpy_s(str, len, s.str);
-//	};
-//
-//	// Assignment operatot (deep Copy + Safty)
-//	MyString operator=(const MyString& RHSArray) {
-//		// Handle self-assignment
-//		if (this != &RHSArray) {
-//			// Free old memory
-//			delete[] str;
-//
-//			//Allocate new memory
-//			int len = strlen(RHSArray.str) + 1;
-//			str = new char[len];
-//
-//			strcpy_s(str, len, RHSArray.str);
-//
-//			/* --- Manual Deep Copuy
-//				for (int i = 0; i < len; i++) {
-//					str[i] = RHSArray.str[i];
-//				}
-//			*/
-//		}
-//
-//		return *this;
-//	}
-//
-//	// destructors
-//	~MyString() { delete[] str; };
-//
-//	void printString() {
-//		cout << str << endl;
-//	};
-//};
-//
-//int main() {
-//
-//	MyString s1;
-//	MyString s2("Test Assignment Operator");
-//	cout << "s1 ";
-//	s1.printString();
-//	cout << "s2 ";
-//	s2.printString();
-//
-//	cout << "\n============== Assingment Operator  ==============\n";
-//	s1 = s2;
-//	cout << "s1 ";
-//	s1.printString();
-//	cout << "s2 ";
-//	s2.printString();
-//
-//
-//	s1 = s1;
-//	cout << "s1 ";
-//	s1.printString();
-//
-//}
+#include <iostream>
+
+using namespace std;
+
+const char defaultStr[] = "Hello world";
+
+class MyString {
+
+private:
+	char* str;
+
+public:
+
+	MyString(const char* input = defaultStr) {
+
+		if (input == nullptr || strlen(input) == 0) {
+			input = defaultStr;
+		}
+
+		int len = strlen(input) + 1;
+		str = new char[len];
+		strcpy_s(str, len, input);
+
+	};
+
+	// Copy Constructor
+	MyString(const MyString& s) {
+		int len = strlen(s.str) + 1;
+		str = new char[len];
+
+		strcpy_s(str, len, s.str);
+	};
+
+	// Assignment operatot (deep Copy + Safty)
+	MyString operator=(const MyString& RHSArray) {
+		// Handle self-assignment
+		if (this != &RHSArray) {
+			// Free old memory
+			delete[] str;
+
+			//Allocate new memory
+			int len = strlen(RHSArray.str) + 1;
+			str = new char[len];
+
+			strcpy_s(str, len, RHSArray.str);
+
+			/* --- Manual Deep Copuy
+				for (int i = 0; i < len; i++) {
+					str[i] = RHSArray.str[i];
+				}
+			*/
+		}
+
+		return *this;
+	}
+
+	// destructors
+	~MyString() { delete[] str; };
+
+	void printString() {
+		cout << str << endl;
+	};
+};
+
+int main() {
+
+	MyString s1;
+	MyString s2("Test Assignment Operator");
+	cout << "s1 ";
+	s1.printString();
+	cout << "s2 ";
+	s2.printString();
+
+	cout << "\n============== Assingment Operator  ==============\n";
+	s1 = s2;
+	cout << "s1 ";
+	s1.printString();
+	cout << "s2 ";
+	s2.printString();
+
+
+	s1 = s1;
+	cout << "s1 ";
+	s1.printString();
+
+}
--- CodeTemplate/FriendFunction.cpp
+++ CodeTemplate/FriendFunction.cpp
@@ -21,99 +21,99 @@
 	 even after output was found, return unexpected data causing errors.
 
 */
-//#include <iostream>
-//
-//using namespace std;
-//
-//class intArray {
-//private:
-//	int size;
-//	int* nums;
-//
-//public:
-//	intArray(int s) {
-//		size = s;
-//		nums = new int[size]{};
-//	}
-//
-//	~intArray() { delete[] nums; };
-//
-//	// Getter
-//	void setIntArray( const int idx, const int value) {
-//		if (idx < 0 || idx >= size) {
-//			cout << "Position value is out of bounds!!!\n" << endl;
-//			return;
-//		}
-//		nums[idx] = value;
-//	}
-//
-//	// Setter
-//	void printArray() const {
-//		cout << " :";
-//		for (int i = 0; i < size; i++) {
-//			cout << " " << *(nums + i);
-//		}
-//		cout << endl;
-//	}
-//
-//	int getMax() const {
-//
-//		int* maxValue = nums;
-//		for (int i = 1; i < size; i++) {
-//			if ( *maxValue < nums[i] ) {
-//				maxValue = &nums[i];
-//			}
-//		}
-//		return *maxValue;
-//	};
-//
-//	friend void swapArray( intArray& a, intArray& b );
-//
-//};
-//
-//void swapArray( intArray& a, intArray& b ) {
-//	int* temp = a.nums;
-//	a.nums = b.nums;
-//	b.nums = temp;
-//
-//	int t = a.size;
-//	a.size = b.size;
-//	b.size = t;
-//};
-//
-//int main() {
-//
-//	intArray a1(3);
-//	a1.setIntArray(0,8);
-//	a1.setIntArray(1, 2);
-//	a1.setIntArray(2, 5);
-//
-//	cout << "a1";
-//	a1.printArray();
-//
-//
-//	intArray a2(5);
-//	a2.setIntArray(0, 28);
-//	a2.setIntArray(1, 12);
-//	a2.setIntArray(2, 85);
-//	a2.setIntArray(3, 200);
-//	a2.setIntArray(4, 82);
-//
-//	cout << "a2";
-//	a2.printArray();
-//	cout << "a1 Max Value: " << a1.getMax() << endl;
-//	cout << "a2 Max Value: " << a2.getMax() << endl;
-//
-//	cout << "\n==============  swap  ================\n" << endl;
-//
-//	swapArray(a1, a2);
-//
-//	cout << "a1";
-//	a1.printArray();
-//
-//	cout << "a2";
-//	a2.printArray();
-//	cout << "a1 Max Value: " << a1.getMax() << endl;
-//	cout << "a2 Max Value: " << a2.getMax() << endl;
-//
-//}
+#include <iostream>
+
+using namespace std;
+
+class intArray {
+private:
+	int size;
+	int* nums;
+
+public:
+	intArray(int s) {
+		size = s;
+		nums = new int[size]{};
+	}
+
+	~intArray() { delete[] nums; };
+
+	// Getter
+	void setIntArray( const int idx, const int value) {
+		if (idx < 0 || idx >= size) {
+			cout << "Position value is out of bounds!!!\n" << endl;
+			return;
+		}
+		nums[idx] = value;
+	}
+
+	// Setter
+	void printArray() const {
+		cout << " :";
+		for (int i = 0; i < size; i++) {
+			cout << " " << *(nums + i);
+		}
+		cout << endl;
+	}
+
+	int getMax() const {
+
+		int* maxValue = nums;
+		for (int i = 1; i < size; i++) {
+			if ( *maxValue < nums[i] ) {
+				maxValue = &nums[i];
+			}
+		}
+		return *maxValue;
+	};
+
+	friend void swapArray( intArray& a, intArray& b );
+
+};
+
+void swapArray( intArray& a, intArray& b ) {
+	int* temp = a.nums;
+	a.nums = b.nums;
+	b.nums = temp;
+
+	int t = a.size;
+	a.size = b.size;
+	b.size = t;
+};
+
+int main() {
+
+	intArray a1(3);
+	a1.setIntArray(0,8);
+	a1.setIntArray(1, 2);
+	a1.setIntArray(2, 5);
+
+	cout << "a1";
+	a1.printArray();
+
+
+	intArray a2(5);
+	a2.setIntArray(0, 28);
+	a2.setIntArray(1, 12);
+	a2.setIntArray(2, 85);
+	a2.setIntArray(3, 200);
+	a2.setIntArray(4, 82);
+
+	cout << "a2";
+	a2.printArray();
+	cout << "a1 Max Value: " << a1.getMax() << endl;
+	cout << "a2 Max Value: " << a2.getMax() << endl;
+
+	cout << "\n==============  swap  ================\n" << endl;
+
+	swapArray(a1, a2);
+
+	cout << "a1";
+	a1.printArray();
+
+	cout << "a2";
+	a2.printArray();
+	cout << "a1 Max Value: " << a1.getMax() << endl;
+	cout << "a2 Max Value: " << a2.getMax() << endl;
+
+}
